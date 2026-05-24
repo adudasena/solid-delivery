c560f458de1af6cd4b423c39faa492b6ef7d4de4
Duda Sena <duda.sruiz@gmail.com>
2026-05-23 21:25:22 -0300

    feat(lsp): criada classe Main para demonstrar a substituicao de Liskov

---
+++ src/main/java/LSP/Main.java
@@ -0,0 +1,18 @@
+package LSP;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Produto> cardapio = new ArrayList<>();
+
+        // Adicionando as subclasses normalmente (Substituição de Liskov)
+        cardapio.add(new Pizza("Frango e Bacon", 49.90));
+        cardapio.add(new Hamburguer("Duplo Smash", 34.50));
+        cardapio.add(new Bebida("Suco Natural de Abacaxi", 9.00));
+
+        for (Produto produto : cardapio) {
+            produto.exibirDetalhes();
+        }
+    }
+}
